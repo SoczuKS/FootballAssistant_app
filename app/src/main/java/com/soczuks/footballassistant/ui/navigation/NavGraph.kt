f15d3e09113bdd357268a7008e014e2edd885dfc
SoczuKS <soczynskik@gmail.com>
2026-09-10 15:38:20 +0200

    Prepare base NavGraph skeleton

--- app/src/main/java/com/soczuks/footballassistant/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/soczuks/footballassistant/ui/navigation/NavGraph.kt
@@ -7,6 +7,17 @@ import androidx.navigation.compose.composable
 
 sealed class Screen(val route: String) {
     object Startup : Screen("startup")
+    object Login : Screen("login")
+    object Register : Screen("register")
+    object Home : Screen("home")
+    object Matches : Screen("matches")
+    object MatchDetails : Screen("match_details/{matchId}") {
+        fun createRoute(matchId: Int) = "match_details/$matchId"
+    }
+    object Competitions : Screen("competitions")
+    object CompetitionDetails : Screen("competition_details/{competitionId}") {
+        fun createRoute(competitionId: Int) = "competition_details/$competitionId"
+    }
 }
 
 @Composable
@@ -16,6 +27,27 @@ fun NavGraph(navController: NavHostController) {
         startDestination = Screen.Startup.route
     ) {
         composable(Screen.Startup.route) {
+
+        }
+        composable(Screen.Login.route) {
+
+        }
+        composable(Screen.Register.route) {
+
+        }
+        composable(Screen.Home.route) {
+
+        }
+        composable(Screen.Matches.route) {
+
+        }
+        composable(Screen.MatchDetails.route) {
+
+        }
+        composable(Screen.Competitions.route) {
+
+        }
+        composable(Screen.CompetitionDetails.route) {
 
         }
     }

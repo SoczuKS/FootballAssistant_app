2133d16ddef9505cdda26ad407c90633478db1ac
SoczuKS <soczynskik@gmail.com>
2026-09-18 15:04:48 +0200

    Change version number -> 0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.soczuks.footballassistant"
         minSdk = 31
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
